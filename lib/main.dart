6651f97967a663d07b360dc1c8235c34b13d7a6e
ROUNAK KUMAR JHA <rounakjha291202.rar@gmail.com>
2022-12-25 19:37:07 +0530

    Integrated Web , and Android platform to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:vybln/responsive/Mobile_Screen_Layout.dart';
 import 'package:vybln/responsive/Web_Screen_Layout.dart';
 import 'package:vybln/responsive/responsive_layout.dart';
 import 'package:vybln/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensures All Flutter widgets are built
+  if (kIsWeb) {
+    //Checking if platform is web
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: 'AIzaSyDWU0hL30JqP1H43ISiXo2AXBqC4pSk06I',
+            appId: '1:919645106149:web:2c6125c022e44a99ea490c',
+            messagingSenderId: '919645106149',
+            projectId: 'vybln-app',
+            storageBucket:
+                'vybln-app.appspot.com')); // Initializing Flutter for mobile apps
+  } else {
+    await Firebase.initializeApp(); // Initializing Flutter for mobile apps
+  }
   runApp(const MyApp());
 }
 
